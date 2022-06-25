d229c9cf2db546937823dc3cd36700b07cff70bd
Gourav Kumar <gourav2711@gmail.com>
2022-06-25 18:50:25 +0530

    removed entrypoint

--- setup.py
+++ setup.py
@@ -3,20 +3,15 @@ from setuptools import find_packages, setup
 license = open("LICENSE").read().strip()
 
 setup(
-    name="PyFacts",
-    version="0.0.1",
+    name="pyfacts",
+    version=open("VERSION").read().strip(),
     license=license,
     author="Gourav Kumar",
     author_email="gouravkr@outlook.in",
     url="https://gouravkumar.com",
-    description="A library which makes handling time series data easier",
+    description="A library to perform financial analytics on Time Series data",
     long_description=open("README.md").read().strip(),
     packages=find_packages(),
     install_requires=["python-dateutil"],
     test_suite="tests",
-    entry_points={
-        "console_scripts": [
-            "fincal=fincal.__main__:main",
-        ]
-    },
 )
